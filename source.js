6f2f800a05250f0c41e526e51be3eb834412b39b
wnyao <wenyao_k@hotmail.com>
2016-12-15 22:02:58 +0800

    Bug fixes and Documentation
    Bug fixes for Object.prototype.getkey, improvement on documentation and
    graph design.

--- source.js
+++ source.js
@@ -10,83 +10,35 @@ window.onload = function(){
   document.getElementById('canvas').innerHTML = drawBar(dataNums, smallLetters); //Display empty chart while webpage onload
 };
 
-function drawBar (data, labels)
+function drawBar (data, labels){
 /* This function will get two arrays as parameters and draw a bar graph using RGraph. */
-{
+  RGraph.Reset(document.getElementById('canvas')); //Reset canvas to avoid overlapping
     var bar = new RGraph.Bar({
         id: 'canvas',
         data: data,
         options: {
             textAccessible: true,
             labels: labels,
-            tooltips: labels
+            tooltips: labels,
+            tooltipsEvent: 'onmousemove',
+            gutterLeft: 75,
+            gutterRight: 5,
+            gutterTop: 20,
+            backgroundGridWidth: 1.2,
+            textSize: 11.5,
+            labelsAbove: true,
+            labelsAboveOffset: 2,
+            title: "Number of Occurrences of Alphabetical Character",
+            titleY: -22,
+            titleBold: true,
+            titleXaxis: "Character",
+            titleXaxisY: 510,
+            titleYaxis: "Frequency",
+            titleYaxisX: 10,
+            bevel: true,
         }
     }).draw()
 }
-
-function drawButton() {
-  /* This function reset canvas and call function of drawBar() */
-  RGraph.Reset(document.getElementById('canvas')); //Reset canvas to avoid overlapping
-  drawBar(dataNums, dataLabels);
-}
-
-function sortButton() {
-  /* This function will store sorted data numbers (number more than zero) within an array and get the
-   * correct letter of each number into an array before passing it into 'drawBar' function
-   */
-  RGraph.Reset(document.getElementById('canvas')); //Reset canvas to avoid overlapping
-  var sortedDataNumbers = sort(data);
-  var sortedDataNumbers1 = [];
-
-  for (var i = 0; i < sortedDataNumbers.length; i++) {
-    if (sortedDataNumbers[i] == 0){ //Continue if occurrence is zero
-      continue;
-    } else {
-      sortedDataNumbers1.push(sortedDataNumbers[i]); //Store data with value more than zero
-      var dataKey = data.getkey(sortedDataNumbers[i]);
-      sortedDatalabels.push(dataKey);
-    }
-  }
-  drawBar(sortedDataNumbers1, sortedDatalabels);
-  document.getElementById('content').innerHTML = "sortedDataNumbers: " + sortedDataNumbers; //testing
-  document.getElementById('content1').innerHTML = "sortedDatalabels: " + sortedDatalabels; //testing
-  sortedDatalabels = []; //Reset array to prevent overlapping labels used to draw graph
-}
-/*
-Object.prototype.getkey = function(value) {
-  /* This prototype property receives a value as parameter and return key that have the similar value
-   * from the object.
-   *
-  var j = 0;
-
-  for (var i = 0; i < smallLetters.length; i++) {
-    if (this[smallLetters[i]] == value) {   //Loop throught object to find the correct key with the same value
-      for (j; j <= sortedDatalabels.length; j++) {
-        if (smallLetters[i] == sortedDatalabels[j]) { //Condition designed to avoid reappearance of keys with similar occurence
-          j++;
-          break;
-        } else {
-          return smallLetters[i]; //Return key that has not been recorded in 'sortedDatalabels'
-        }
-      }
-    }
-  }
-};
-*/
-
-Object.prototype.getkey = function(value) {
-  /* This prototype property receives a value as parameter and return key that have the similar value
-   * from the object.
-   */
-  var j = 0;
-
-  for (var i = 0; i < smallLetters.length; i++) {
-    if (this[smallLetters[i]] == value) {   //Loop throught object to find the correct key with the same value
-      return smallLetters[i]; //Return key that has not been recorded in 'sortedDatalabels'
-    }
-  }
-};
-
 
 function readFile(file){
   /* This function will first read file and alert user whether is file input loaded successfully. While
@@ -122,6 +74,69 @@ function readFile(file){
   }
 }
 
+function drawButton() {
+  /* This function reset canvas and call function of drawBar() */
+  drawBar(dataNums, dataLabels);
+}
+
+function sortButton() {
+  /* This function will store sorted data numbers (number more than zero) within an array and get the
+   * correct letter of each number into an array before passing it into 'drawBar' function
+   */
+  var sortedDataNumbers = sort(data);
+  var sortedDataNumbers1 = [];
+  sortedDatalabels = []; //Reset array to clear unncessary data
+
+  for (var i = 0; i < sortedDataNumbers.length; i++) {
+    if (sortedDataNumbers[i] == 0){ //Continue if occurrence is zero
+      continue;
+    } else {
+      sortedDataNumbers1.push(sortedDataNumbers[i]); //Store data with value more than zero
+      var dataKey = data.getkey(sortedDataNumbers[i]);
+      sortedDatalabels.push(dataKey);
+    }
+  }
+  drawBar(sortedDataNumbers1, sortedDatalabels);
+}
+
+Object.prototype.getkey = function(value) {
+  /* This prototype property receives a value as parameter and return the correct key from object. */
+  for (var i = 0; i < smallLetters.length; i++) {
+    if (this[smallLetters[i]] == value) {   //Loop throught object to find the correct key (letter) with the same value
+      for (var j = 0; j <= sortedDatalabels.length; j++) {
+        if (j == sortedDatalabels.length){
+          return smallLetters[i]; //Return letter if only the letter has not been used
+        } else if (smallLetters[i] == sortedDatalabels[j]) {
+          break; //Break the loop if letter is used before the next iteration of variable 'i'
+        } else if (smallLetters[i] != sortedDatalabels[j]) { //Condition to ensure no similar letter within array of sortedDatalabels
+          continue; //Continue if letter is not found within sortedDatalabels
+        }
+      }
+    }
+  }
+};
+
+/*
+Object.prototype.getkey = function(value) {
+  /* This prototype property receives a value as parameter and return key that have the similar value
+   * from the object.
+   *
+  var j = 0;
+
+  for (var i = 0; i < smallLetters.length; i++) {
+    if (this[smallLetters[i]] == value) {   //Loop throught object to find the correct key with the same value
+      for (j; j <= sortedDatalabels.length; j++) {
+        if (smallLetters[i] == sortedDatalabels[j]) { //Condition designed to avoid reappearance of keys with similar occurence
+          j++;
+          break;
+        } else {
+          return smallLetters[i]; //Return key that has not been recorded in 'sortedDatalabels'
+        }
+      }
+    }
+  }
+};*/
+
 String.prototype.count = function(letter) {
   /* This prototype property receives a letter as parameter and counts the occurence of the letter
    * within a string and return the count.
@@ -145,10 +160,6 @@ function sort(dict) {
       sortedValues1.push(dict[smallLetters[i]]); //Storing dictionary values into an array
   }
   sortedValues1.sort(function(a, b){return b-a}); //Sort number in descending order
-  /*
-  for (var j = sortedValues1.length - 1; j >= 0; j--){
-    	sortedValues2.push(sortedValues1[j]); //Reverse storing of data from sortedValues1
- 	}*/
   return sortedValues1;
 }
 
